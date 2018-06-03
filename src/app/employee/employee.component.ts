de053b149a978554e77a1bc6fbccb666fe38a551
Buddhi Adhikari <buddhiadikari@live.com>
2018-06-03 20:39:29 +0100

    Added DB update functions

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 // local import
 import { EmployeeService } from '../shared/employee.service';
-import { NgForm } from '@angular/forms';
+import { Employee } from '../shared/employee.model';
 
 declare var M: any;
 
@@ -17,6 +19,7 @@ export class EmployeeComponent implements OnInit {
 
   ngOnInit() {
     this.resetForm();
+    this.refreshEmployeeList();
   }
 
   // '?' means the parameter is nullable (can be null)
@@ -36,10 +39,31 @@ export class EmployeeComponent implements OnInit {
   // on form submission
   onSubmit(form: NgForm) {
     // res = respond
-    this.employeeService.postEmployee(form.value).subscribe((res) => {
-      this.resetForm();
-      M.toast({ html: 'Saved succesfully ! ', classes: 'rounded' });
+    if (form.value._id === '') {
+      this.employeeService.postEmployee(form.value).subscribe((res) => {
+        this.resetForm(form);
+        this.refreshEmployeeList();
+        M.toast({ html: 'Saved succesfully ! ', classes: 'rounded' });
+      });
+    } else {
+        this.employeeService.putEmployee(form.value).subscribe((res) => {
+          this.resetForm(form);
+          this.refreshEmployeeList();
+          M.toast({ html: 'Update succesfully ! ', classes: 'rounded' });
+      });
+    }
+  }
+
+  // get all employees on refresh
+  refreshEmployeeList() {
+    this.employeeService.getEmployeeList().subscribe((res) => {
+      // const data = res as Employee[];
+      this.employeeService.employees = res as Employee[];
+      // M.toast({ html: 'Data ' + data[2].name, classes: 'rounded' });
     });
   }
 
+  onEdit(emp: Employee) {
+    this.employeeService.selectedEmployee = emp;
+  }
 }
--- src/app/shared/employee.service.ts
+++ src/app/shared/employee.service.ts
@@ -15,13 +15,26 @@ export class EmployeeService {
   // selectedEmployee var can be used for findbyid, update and delete functions
   selectedEmployee: Employee;
   // the employees list from the db
-  employess: Employee[];
+  employees: Employee[];
   // the path to access the db employees collection
   readonly baseURL = 'http://localhost:3000/employees';
 
   constructor(private http: HttpClient) { }
 
+  // sends user entered data of the form to the db using express & nodejs with
+  // the help of CORS middleware in backend
   postEmployee(emp: Employee) {
     return this.http.post(this.baseURL, emp);
   }
+
+  // gets all te employees from the db through express
+  getEmployeeList() {
+    return this.http.get(this.baseURL);
+  }
+
+  // updates employees in the db
+  putEmployee(emp: Employee) {
+    return this.http.put(this.baseURL + `/${ emp._id }`, emp);
+  }
+
 }
